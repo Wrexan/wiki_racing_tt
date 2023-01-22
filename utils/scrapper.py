b3089c06da734e48571343addc9da6fdd369379f
Wrexan <fwrexan@gmail.com>
2023-01-22 20:56:00 +0200

    refactoring, save before blackout

--- utils/scrapper.py
+++ utils/scrapper.py
@@ -9,24 +9,54 @@ class Scrapper:
     def __init__(self, requests_per_minute: int = 60):
         self.delay_between_requests: float = 1 / (requests_per_minute / 60)
         self.last_request_time: float = time.time()
+        self.soup = None
+        # self.page_dom_path = ('body',
+        #                       ('div', {'id': 'bodyContent'}),
+        #                       ('div', {'class': 'mw-parser-output'}))
+        # self.dom_disassemble_template = self.parse_dom_template()
 
     def scrap_for_linked_pages(self,
                                url: str,
                                href_mask: Pattern,
-                               limit: int) -> tuple:
+                               limit: int) -> list:
         self.make_delay_before_next_request()
         r = requests.get(url)
-        soup = BeautifulSoup(r.text, 'html.parser')
+        self.soup = BeautifulSoup(r.text, 'html.parser')
         links = []
-        raw_links = soup.find('body') \
-            .find('div', id='bodyContent') \
-            .find('div', class_='mw-parser-output') \
-            .find_all('a', href=href_mask, class_=None, limit=limit)
+
+        raw_html = self.soup.find('body')
+        if not raw_html:
+            print(f'Scrapper error: URL have no links: {url}')
+
+        raw_html = raw_html.find('div', id='bodyContent')
+        if not raw_html:
+            print(f'Scrapper error: URL have no id="bodyContent": {url}')
+
+        raw_html = raw_html.find('div', class_='mw-parser-output')
+        if not raw_html:
+            print(f'Scrapper error: URL have no class_="mw-parser-output": {url}')
+
+        raw_links = raw_html.find_all('a', href=href_mask, class_=None, limit=limit)
+        if not raw_links:
+            print(f'Scrapper error: URL have no links: {url}')
 
         for link in raw_links:
-            links.append((link.get('title'),))
-            # links.append((link.get('title'), unquote(link.get('href'))))
-        return *links,
+            links.append(link.get('title'))
+        return links
+
+    # def disassemble_dom(self, url):
+    #     ...
+    #
+    # def parse_dom_template(self, page_dom_path):
+    #     dom_disassemble_template = []
+    #     for page_elem in page_dom_path:
+    #         if isinstance(page_elem, str):
+    #             dom_disassemble_template.append(self.find_page_elem())
+    #
+    #     return
+    #
+    # def find_page_elem(self, name):
+    #     return self.soup.find(name)
 
     def make_delay_before_next_request(self) -> None:
         # current time
--- wikiracing.py
+++ wikiracing.py
@@ -20,14 +20,12 @@ class WikiRacer:
         self.current_deepness = 0
         self.max_deepness = 5
         self.finish_page_name: str = ''
-        self.tree_cache: dict[int: dict[tuple[str]: tuple[...]]] = {}
+        self.tree_cache: dict[int: dict[str: list[str, ...]]] = {}
 
         self.result_found = False
 
     def find_path(self, start: str, finish: str) -> List[str]:
-        # self.current_branch.append(start)
-        # self.tree_cache[self.current_deepness] = {(start,): []}
-        self.tree_cache[self.current_deepness] = {(): ((start,),)}
+        self.tree_cache[self.current_deepness] = {None: [start]}
         # self.tree_cache = {1: {start: [second, third]}, 2: {second: [fourth, fifth], third: [sixth, seventh]}}
         # self.current_deepness = 2
         self.finish_page_name = finish
@@ -70,7 +68,7 @@ class WikiRacer:
                 for link in links:
                     print(f'{link=} ')
 
-                    pages = self.get_pages(link[0])
+                    pages = self.get_links_from_db_or_parser(link)
                     # {1: {start: [second, third]}, 2: {second: [fourth, fifth], third: [sixth, seventh]}}
                     if not self.tree_cache.get(inner_deepness+1):
                         self.tree_cache[inner_deepness+1] = {}
@@ -80,23 +78,36 @@ class WikiRacer:
                     # print(f'-BRANCH={self.current_branch}')
                     # print(f'-TREE={self.tree_cache}')
                     # print(f'-PAGES={pages}\n')
-                    if not self.result_found:
-                        for page_to_check in pages:
-                            if page_to_check[0] == self.finish_page_name:
-                                print(f'FOUND {page_to_check[0]}')
 
-                                self.result_branch.append(self.finish_page_name)
-                                for revers_deepness in range(inner_deepness+1, 0, -1):
-                                    print(f'+++{revers_deepness=} {page_to_check=}')
-                                    for page, links in self.tree_cache[revers_deepness].items():
-                                        if page_to_check in links:
-                                            page_to_check = page
-                                            self.result_branch.append(page_to_check[0])
-                                self.result_branch.reverse()
-                                self.result_found = True
-                                break
-                    # if self.result_found:
-                    #     break
+                    # if not self.result_found:
+                    #     for page_to_check in pages:
+                    #         if page_to_check == self.finish_page_name:
+                    #             print(f'FOUND {page_to_check}')
+                    #
+                    #             self.result_branch.append(self.finish_page_name)
+                    #             for revers_deepness in range(inner_deepness+1, 0, -1):
+                    #                 print(f'+++{revers_deepness=} {page_to_check=}')
+                    #                 for backward_page, backward_links in self.tree_cache[revers_deepness].items():
+                    #                     if page_to_check in backward_links:
+                    #                         page_to_check = backward_page
+                    #                         self.result_branch.append(page_to_check)
+                    #             self.result_branch.reverse()
+                    #             self.result_found = True
+                    #             break
+
+                    if link == self.finish_page_name:
+                        print(f'FOUND {link}')
+
+                        self.result_branch.append(self.finish_page_name)
+                        for revers_deepness in range(inner_deepness+1, 0, -1):
+                            print(f'+++{revers_deepness=} {link=}')
+                            for backward_page, backward_links in self.tree_cache[revers_deepness].items():
+                                if link in backward_links:
+                                    page_to_check = backward_page
+                                    self.result_branch.append(page_to_check)
+                        self.result_branch.reverse()
+                        self.result_found = True
+                        # break
 
                 if self.result_found:
                     break
@@ -105,19 +116,19 @@ class WikiRacer:
                 break
         return
 
-    def get_pages(self, current_page_name):
-        cached_id = self.db.get_page_id_if_cached(self.table_name, current_page_name)
+    def get_links_from_db_or_parser(self, current_page):
+        cached_id = self.db.get_page_id_if_cached(self.table_name, current_page)
         if cached_id:
             pages = self.db.get_related_pages(self.table_name, cached_id)
             print(f'{self.current_deepness}-DBS: ', end='')
         else:
-            current_uri = f'{self.uri_to_parse}{current_page_name}'
+            current_uri = f'{self.uri_to_parse}{current_page}'
             pages = self.scrapper.scrap_for_linked_pages(
                 url=f'{self.site_to_parse}{current_uri}',
                 href_mask=self.href_mask,
                 limit=links_per_page)
 
-            self.db.cache_pages_relations(self.table_name, (current_page_name,), pages)
+            self.db.cache_pages_relations(self.table_name, (current_page,), pages)
             print(f'{self.current_deepness}-URL: ', end='')
         return pages
 
