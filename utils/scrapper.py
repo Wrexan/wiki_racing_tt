5fde9b420fdc812ea6596185347d96535ddcc3fe
Wrexan <fwrexan@gmail.com>
2023-01-22 02:29:32 +0200

    project in process :\ still not working tree search algorythm

--- utils/scrapper.py
+++ utils/scrapper.py
@@ -1,17 +1,42 @@
+import time
+
+import requests
 from bs4 import BeautifulSoup
 from re import Pattern
 
 
-def scrap_for_linked_pages(soup: BeautifulSoup,
-                           href_mask: Pattern,
-                           limit: int) -> list:
-    links = {}
-    raw_links = soup.find('body') \
-        .find('div', id='bodyContent') \
-        .find('div', id='mw-content-text') \
-        .find_all('a', href=href_mask, class_=None, limit=limit)
+class Scrapper:
+    def __init__(self, requests_per_minute: int = 60):
+        self.delay_between_requests: float = 1 / (requests_per_minute / 60)
+        self.last_request_time: float = time.time()
 
-    for link in raw_links:
-        links[link.get('title')] = (link.get('href'))
-    print(links)
-    return links
+    def scrap_for_linked_pages(self,
+                               url: str,
+                               href_mask: Pattern,
+                               limit: int) -> tuple:
+        self.make_delay_before_next_request()
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        links = []
+        raw_links = soup.find('body') \
+            .find('div', id='bodyContent') \
+            .find('div', class_='mw-parser-output') \
+            .find_all('a', href=href_mask, class_=None, limit=limit)
+
+        for link in raw_links:
+            links.append((link.get('title'),))
+            # links.append((link.get('title'), unquote(link.get('href'))))
+        return *links,
+
+    def make_delay_before_next_request(self) -> None:
+        # current time
+        current_time = time.time()
+        # calculate time left until next allowed request
+        delay = self.last_request_time + self.delay_between_requests - current_time
+        # updating time of the next request
+        self.last_request_time = current_time + delay
+        # make delay for the time left until next allowed request
+        if delay > 0:
+            time.sleep(delay)
+        else:
+            print(f'LAG: {-delay=} sec')
--- wikiracing.py
+++ wikiracing.py
@@ -1,13 +1,7 @@
 import re
-import time
 from typing import List
-
-import psycopg2
-import requests
-from bs4 import BeautifulSoup
-
-from utils.db_controller import create_page_names_table, create_page_relations_table
-from utils.scrapper import scrap_for_linked_pages
+from utils.db_controller import DBController
+from utils.scrapper import Scrapper
 
 requests_per_minute = 100
 links_per_page = 20
@@ -15,79 +9,210 @@ links_per_page = 20
 
 class WikiRacer:
     def __init__(self):
-        self.connection = None
-        self.cursor = None
-        self.connection: psycopg2.connection
-        self.cursor: psycopg2.cursor
-        self.delay_between_requests: float = 1 / (requests_per_minute / 60)
-        self.last_request_time: float = time.time()
         self.site_to_parse = 'https://uk.wikipedia.org'
         self.uri_to_parse = '/wiki/'
-        self.href_mask = re.compile(rf'{self.uri_to_parse}*')
+        # self.ignored_title_mask = (re.compile(r'^Спеціальна:*'), re.compile(r'^Вікіпедія:*'))
+        self.href_mask = re.compile(rf'^{self.uri_to_parse}*')
+        self.table_name = 'pages'
+        self.db = DBController()
+        self.scrapper = Scrapper(requests_per_minute=requests_per_minute)
+        self.current_branch: list = []
+        self.current_deepness = 0
+        self.max_deepness = 3
+        self.finish_page_name: str = ''
+        self.tree_cache: {int: list} = {}
+
+        self.SEARCH = 0
+        self.RESULT_FOUNDED = 1
+        self.FINISHED = 2
+        self.status = self.SEARCH
 
     def find_path(self, start: str, finish: str) -> List[str]:
+        self.current_branch.append(start)
+        self.tree_cache[self.current_deepness] = [(start,)]
+        # self.current_deepness = 2
+        self.finish_page_name = finish
 
-        # self.connection = self.create_connection()
-        # if not self.connection:
-        #     return []
-        # self.cursor = self.connection.cursor()
-        # create_page_names_table(conn_source=self, table_name='pages')
-        # create_page_relations_table(conn_source=self, page_table_name='pages')
+        self.db.create_connection(
+            host="localhost",
+            database="cash_db",
+            user="postgres",
+            port=5433,
+            password="123")
+        if not self.db.connection:
+            return []
 
-        links: list = self.get_parsed_links(current_page_name=start, finish_page_name=finish)
-        # links: list = []
-        # for link_number in range(links_per_page):
-        #     links.append(self.get_parsed_links(start=start, finish=finish))
-        # self.cursor.close()
-        # self.connection.close()
+        self.db.create_page_names_table(table_name=self.table_name)
+        self.db.create_m2m_relations_table(table_name=self.table_name)
+
+        for step in range(self.max_deepness):
+            self.current_deepness = step
+            self.get_parsed_links(page=(start,))
+
+        if self.status == self.RESULT_FOUNDED:
+            print(f'FOUND RESULT = {self.current_branch}')
+        else:
+            print(f'NOT FOUND. LAST RESULT = {self.current_branch}')
+
+        self.db.cursor.close()
+        self.db.connection.close()
         # print(f'{links=}')
         # return links
 
-    @staticmethod
-    def create_connection():
-        connection = None
-        try:
-            connection = psycopg2.connect(
-                host="localhost",
-                database="cash_db",
-                user="postgres",
-                port=5433,
-                password="123")
-        except Exception as err:
-            print(f'Error: {err}')
-        return connection
+    def get_parsed_links(self, page: tuple, in_cycle: bool = False):
 
-    def get_parsed_links(self, current_page_name: str, finish_page_name: str) -> list:
-        self.make_delay_before_next_request()
-        r = requests.get(f'{self.site_to_parse}{self.uri_to_parse}{current_page_name}')
-        soup = BeautifulSoup(r.text, 'html.parser')
-        links = scrap_for_linked_pages(soup=soup,
-                                       href_mask=self.href_mask,
-                                       limit=links_per_page)
+        if self.status == self.FINISHED:
+            return
 
-        for link in links:
+        self.current_branch[-1] = page[0]
 
-        #     found = self.get_parsed_links(page_name, finish_page_name)
-        #     if found:
-        #         print('FOUND')
-        # if title.lower() == finish_page_name.lower():
-        #     print('FOUND')
-        # return [current_page_name, title]
+        # getting current page from branch
+        pages = self.get_pages(current_page_name=page[0])
+        # print(f'{pages=}')
 
-        return links
+        overstep = 1 if not in_cycle else 2
+        if not self.tree_cache.get(self.current_deepness+overstep):
+            print(f'extending')
+            self.tree_cache[self.current_deepness+overstep] = []
+        self.tree_cache[self.current_deepness+overstep].extend(pages)
 
-    def make_delay_before_next_request(self) -> None:
-        # current time
-        current_time = time.time()
-        # calculate time left until next allowed request
-        delay = self.last_request_time + self.delay_between_requests - current_time
-        # updating time of the next request
-        self.last_request_time = current_time + delay
-        # make delay for the time left until next allowed request
-        if delay > 0:
-            time.sleep(delay)
+        # print(f'TREE={self.tree_cache.get(self.current_deepness)}')
+        print(f'-BRANCH={self.current_branch}')
+        print(f'-TREE={self.tree_cache}')
+        # print(f'-PAGES={pages}\n')
+
+        for page_to_check in pages:
+            if page_to_check == self.finish_page_name:
+                print(f'FOUND {page_to_check=}')
+                print(f'{self.current_branch},{self.finish_page_name}')
+                self.current_branch.append(self.finish_page_name)
+                self.status = self.RESULT_FOUNDED
+
+        # returning only after full page scan
+        if self.status == self.RESULT_FOUNDED:
+            self.status = self.FINISHED
+            return
+
+        if not in_cycle:
+            self.current_branch.append(None)
+            # print(f'{self.tree_cache[self.current_deepness]=}')
+            for next_page in self.tree_cache[self.current_deepness+1]:
+                print(f'{next_page=}')
+                self.get_parsed_links(page=next_page, in_cycle=True)
+
+        # if self.current_deepness > self.max_deepness:
+        #     self.status = self.FINISHED
+        #     return
+
+        # self.current_deepness += 1
+
+
+        # print(f'-ALL={self.tree_cache}\n')
+        # for next_page in self.tree_cache[self.current_deepness-1]:
+        #     self.get_parsed_links(page=next_page, go_deeper=True)
+
+        # if not self.dependencies_tree_cache.get(self.current_deepness):
+        #     print(f'extending')
+        #     self.dependencies_tree_cache[self.current_deepness] = []
+        # self.dependencies_tree_cache[self.current_deepness].extend(pages)
+
+        # if go_deeper:
+        #     print(f'{"=" * self.current_deepness}> going deeper')
+        #     self.current_branch.append(None)
+        #
+        #     self.current_deepness += 1
+        #     for link in pages:
+        #         self.current_branch[-1] = link[0]
+        #         pages = pages or self.get_pages(current_page_name=self.current_branch[-1])
+        #         self.get_parsed_links(pages=pages, go_deeper=False)
+        #
+        #     if self.current_deepness > self.max_deepness:
+        #         self.status = self.FINISHED
+        #         return
+
+            # self.dependencies_tree_cache[self.current_deepness] = []
+            #
+            # self.current_pages_branch.append(None)
+            #
+            # print(f'{self.dependencies_tree_cache=}')
+            # for branch in self.dependencies_tree_cache[self.current_deepness - 1]:
+            #     print(f'{branch[0]=}')
+            #     self.current_pages_branch[-1] = branch[0]
+            #     self.get_parsed_links(go_deeper=True)
+        return
+
+    def get_pages(self, current_page_name):
+        cached_id = self.db.get_page_id_if_cached(self.table_name, current_page_name)
+        if cached_id:
+            pages = self.db.get_related_pages(self.table_name, cached_id)
+            print(f'{self.current_deepness}-DBS: ', end='')
         else:
-            print(f'LAG: {-delay=} sec')
+            current_uri = f'{self.uri_to_parse}{current_page_name}'
+            pages = self.scrapper.scrap_for_linked_pages(
+                url=f'{self.site_to_parse}{current_uri}',
+                href_mask=self.href_mask,
+                limit=links_per_page)
+
+            self.db.cache_pages_relations(self.table_name, (current_page_name,), pages)
+            print(f'{self.current_deepness}-URL: ', end='')
+        return pages
+
+    # def get_parsed_links2(self, go_deeper: bool = True):
+    #     if self.status == self.FINISHED:
+    #         return
+    #     # getting current page from branch
+    #     current_page_name = self.current_pages_branch[-1]
+    #     current_uri = f'{self.uri_to_parse}{current_page_name}'
+    #
+    #     cached_id = self.db.get_page_id_if_cached(self.table_name, current_page_name)
+    #     if cached_id:
+    #         pages = self.db.get_related_pages(self.table_name, cached_id)
+    #         print('DBS: ', end='')
+    #     else:
+    #         pages = self.scrapper.scrap_for_linked_pages(
+    #             url=f'{self.site_to_parse}{current_uri}',
+    #             href_mask=self.href_mask,
+    #             limit=links_per_page)
+    #
+    #         self.db.cache_pages_relations(self.table_name, (current_page_name,), pages)
+    #         print('URL: ', end='')
+    #
+    #     print(f'{self.current_pages_branch=}')
+    #     # print(f'{pages=}')
+    #
+    #     for page in pages:
+    #         if page == self.finish_page_name:
+    #             print(f'FOUND {page=}')
+    #             print(f'{self.current_pages_branch},{self.finish_page_name}')
+    #             self.current_pages_branch.append(self.finish_page_name)
+    #             self.status = self.RESULT_FOUNDED
+    #
+    #     # returning only after full page scan
+    #     if self.status == self.RESULT_FOUNDED:
+    #         self.status = self.FINISHED
+    #         return
+    #
+    #     if go_deeper:
+    #         print(f'{"=" * self.current_deepness}> going deeper')
+    #         self.current_pages_branch.append(None)
+    #         for link in pages:
+    #             self.current_pages_branch[-1] = link[0]
+    #             self.get_parsed_links(go_deeper=False)
+    #
+    #         if self.current_deepness <= self.max_deepness:
+    #             self.current_deepness += 1
+    #             self.dependencies_tree_cache[self.current_deepness] = pages
+    #
+    #             print(f'{self.dependencies_tree_cache=}')
+    #             for branch in self.dependencies_tree_cache[self.current_deepness]:
+    #                 print(f'{branch[0]=}')
+    #                 self.current_pages_branch[-1] = branch[0]
+    #                 self.current_pages_branch.append(None)
+    #                 self.get_parsed_links(go_deeper=False)
+    #
+    #             self.get_parsed_links(go_deeper=True)
+    #
+    #     return
 
 
 if __name__ == '__main__':
